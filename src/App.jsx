0cac1492019002cc7a70e166690c8f308a66f055
Habib <habibulislam6862@gmail.com>
2021-07-08 23:08:22 +0600

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import React, { Component } from "react";
 import './App.css'
 import Profile from "./components/Profile";
 
+
 class App extends Component {
+  
   render() {
     return (
+      <>
       <Profile/>
+      </>
     );
   }
 }
